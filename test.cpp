c846ba9141f6a221d2c8802bed22a3bf06984328
bigheadofliu <871314635@qq.com>
2025-05-09 16:12:53 +0800

    问题修复

--- test.cpp
+++ test.cpp
@@ -1,14 +1,14 @@
 #include "ThreadPool.hpp"
 #include "chrono"
 void test_func(int id){
-    std::cout << "Thread " << std::this_thread::get_id()
-    << " is working on task " << id << std::endl;
-    std::this_thread::sleep_for(std::chrono::milliseconds(100)); // 模拟工作
+    std::cout << "Thread " << std::this_thread::get_id()<< " is working on task: " << id << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000)); // 模拟工作 100太快改为200
     }
 
     int main(){
-        ThreadPool pool(2,6);
-        for(auto i=0;i<20;i++){
+        ThreadPool pool(10,20);
+        // std::this_thread::sleep_for(std::chrono::milliseconds(200)); // 等线程初始化完 没有卵用啊
+        for(auto i=0;i<30;i++){
             int task_id=i;
             pool.AddTask(Task([task_id](){test_func(task_id);}));
         }
